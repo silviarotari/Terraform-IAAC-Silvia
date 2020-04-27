1c95aed4bcc2d6fe6e88996de264e85abeb64094
silviarotari <rotarisilvia92@gmail.com>
2020-04-27 17:39:37 +0000

    added

---
+++ APACHE/user_data.sh
@@ -0,0 +1,7 @@
+#user_data
+#!/bin/bash
+sudo yum install httpd -y 
+sudo systemctl enable httpd
+sudo systemctl start httpd
+
+echo "<h1>httpd-user_data with Terraform</h1>" > /var/www/html/index.html
