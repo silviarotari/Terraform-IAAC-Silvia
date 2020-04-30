0fe0dad44299f24ed0a79ab3498500f8e6f534d7
silviarotari <57469926+silviarotari@users.noreply.github.com>
2020-04-30 07:38:01 -0500

    Update user_data.sh

--- Task1/user_data.sh
+++ Task1/user_data.sh
@@ -4,3 +4,21 @@ sudo systemctl enable httpd
 sudo systemctl start httpd
 
 echo "<h1>httpd-user_data with Terraform</h1>" > /var/www/html/index.html
+
+
+
+##
+#!/bin/bash
+yum update -y
+amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+yum install -y httpd mariadb-server
+systemctl start httpd
+systemctl enable httpd
+chown -R apache:apache /var/www/html
+echo "<!DOCTYPE html>
+<html>
+<head><center><b>Apache is Deployed via Terraform</b></center></head>
+<body><center>
+<img src="https://s24255.pcdn.co/wp-content/uploads/2015/03/Apache-http-server.png">
+</center></body>
+</html>" > /var/www/html/index.html
