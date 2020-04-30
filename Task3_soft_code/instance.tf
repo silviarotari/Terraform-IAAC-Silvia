7b781c1ad77a2b99005408fce5ed0e9fc236c556
silviarotari <rotarisilvia92@gmail.com>
2020-04-30 20:28:58 +0000

    added

--- Task3_soft_code/instance.tf
+++ Task3_soft_code/instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
   ami           = "${var.ami}"
   instance_type = "${var.instance_type}"
   key_name      = "${aws_key_pair.us-east-1-key.key_name}"
-  security_groups = ["${aws_security_group.allow_tls.name}"]
+  subnet_id = "${aws_subnet.public_subnet1.id}"
   user_data = "${file("user_data.sh")}"
   availability_zone = "${var.public_subnet_az}"
   vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
