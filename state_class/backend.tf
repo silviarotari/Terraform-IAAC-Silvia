a3ce5c608abae7f4c17466a373d626e3501c7fe0
silviarotari <rotarisilvia92@gmail.com>
2020-05-01 10:19:24 +0000

    added

--- state_class/backend.tf
+++ state_class/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "0.11.14"
   backend "s3" {
     bucket = "state-class-silvia"
-    key    = "path/to/my/key"
+    key    = "projects/us-east-1/tools/terraform.tfstate"
     //dynamodb_table = "state_class"
     region = "us-east-1"
   }
