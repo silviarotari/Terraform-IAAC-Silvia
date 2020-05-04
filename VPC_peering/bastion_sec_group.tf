2dc4670a2f2d706179d4b0ed34f58c6336d99249
silviarotari <rotarisilvia92@gmail.com>
2020-05-04 03:34:25 +0000

    added

---
+++ VPC_peering/bastion_sec_group.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "bastion_sec_group" {
+  name        = "${var.sg_name}"
+  description = "Allow TLS inbound traffic"
+  vpc_id = "${aws_vpc.main.id}" 
+  
+  
+  tags = "${var.tags}"
+ 
+  
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
