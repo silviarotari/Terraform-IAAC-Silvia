c60d1f805d7b5132e93fcb1e3c57fe9afc6b17c9
silviarotari <rotarisilvia92@gmail.com>
2020-05-04 03:19:30 +0000

    added

--- VPC_peering/rt_private_sub.tf
+++ VPC_peering/rt_private_sub.tf
@@ -4,7 +4,7 @@ resource "aws_route_table" "rt_private" {
   tags = "${var.tags}"
   route { 
     cidr_block = "0.0.0.0/0" 
-    gateway_id = "${aws_nat_gateway.mat.id}" 
+    gateway_id = "${aws_nat_gateway.ngw.id}" 
     } 
 } 
 
