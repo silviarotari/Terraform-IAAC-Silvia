74aa7f68c3bfa98d082a78d868be09ce3f730cad
silviarotari <rotarisilvia92@gmail.com>
2020-05-06 21:39:40 +0000

    added

---
+++ terraform-aws-asg/ex10.key_pair_another_regions/app/asg_module.tf
@@ -0,0 +1,18 @@
+module "ohio" { 
+source = "./module" 
+region = "us-east-2" 
+} 
+
+ 
+
+module "oregon" { 
+source = "./module" 
+region = "us-west-2" 
+} 
+
+ 
+
+module "london" { 
+source = "./module" 
+region = "eu-west-2" 
+} 
---
+++ terraform-aws-asg/ex10.key_pair_another_regions/app/module/key_pair.tf
@@ -0,0 +1,5 @@
+resource "aws_key_pair" "us-east-1-key" { 
+key_name = "asg-key-pair" 
+public_key = "${file("~/.ssh/id_rsa.pub")}" 
+
+} 
