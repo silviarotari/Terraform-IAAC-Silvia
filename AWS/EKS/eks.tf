09225677deec2bd58d45d0303c02aa9f50d58378
silviarotari <rotarisilvia92@gmail.com>
2020-05-12 00:13:30 +0000

    added

---
+++ AWS/EKS/eks.tf
@@ -0,0 +1,32 @@
+data "aws_eks_cluster" "cluster" { 
+  name = module.my-cluster.cluster_id 
+} 
+
+ 
+data "aws_eks_cluster_auth" "cluster" { 
+  name = module.my-cluster.cluster_id 
+} 
+
+ 
+provider "kubernetes" { 
+  host = data.aws_eks_cluster.cluster.endpoint 
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data) 
+  token = data.aws_eks_cluster_auth.cluster.token 
+  load_config_file = false 
+  version = "~> 1.9" 
+} 
+
+ 
+module "my-cluster" { 
+  source = "terraform-aws-modules/eks/aws" 
+  cluster_name = "my-cluster" 
+  cluster_version = "1.14" 
+  subnets = [
+      "subnet-07f93923e4dd4c44c", "subnet-0bd918a8c7f0fa5c7", "subnet-0a18452194a979a1b"] 
+  vpc_id = "vpc-06200fe3216ddcc75" 
+  worker_groups = [{ 
+    instance_type = "m4.large" 
+    asg_max_size = 5 
+    } 
+  ] 
+} 
--- AWS/provider.tf
+++ AWS/provider.tf
@@ -1,6 +1,6 @@
-provider "aws" { 
-region = "us-east-1" 
-} 
+provider "aws" {
+  region = "us-east-1"
+}
 
 //Whitch provider to work with
 //Whitch region to work with
